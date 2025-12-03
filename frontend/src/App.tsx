72796c332243b62b560c5016b971580df087e244
metisssi <simcheraadnrij@gmail.com>
2025-12-03 16:46:44 +0100

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-8">
+      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-secondary ml-4">Another button</button>
+    </div>
+  );
+}
+
+export default App;
